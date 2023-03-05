d5a5ecbdfa493b35f51e4fd016efa942c84a153f
anvarsaidov <anvar5737251saidov@gmail.com>
2023-03-05 18:38:39 +0300

    Initial Commit

---
+++ testLast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testLast
+//
+//  Created by AnvarSaidov on 5.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
